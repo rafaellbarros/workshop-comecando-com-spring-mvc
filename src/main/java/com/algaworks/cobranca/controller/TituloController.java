df44f47925723f272b6eaa33fa72e7dc1af03599
Rafael Barros <rafaelbarros.df@gmail.com>
2019-06-12 00:09:42 -0300

    :construction: 2.6 - montando combo dinamicamente

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,12 +1,17 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
+import com.algaworks.cobranca.model.enums.StatusTitulo;
 import com.algaworks.cobranca.repository.TituloRepository;
 
 @Controller
@@ -17,8 +22,9 @@ public class TituloController {
 	private TituloRepository tituloRepository;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -27,6 +33,11 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título salvo com sucesso!");
 		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 	
 }
