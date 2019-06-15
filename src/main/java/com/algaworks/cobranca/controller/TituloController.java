d0cefc52e19a2beaab9dd6e0ee9824fab587599d
Rafael Barros <rafaelbarros.df@gmail.com>
2019-06-14 23:55:41 -0300

    :construction: 2.14 - mantendo valores no formulario

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.model.enums.StatusTitulo;
@@ -39,16 +40,16 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+
 		if (errors.hasErrors()) {
-			return mv;
+			return "CadastroTitulo";
 		}
 		
 		tituloRepository.save(titulo);
 		
-		mv.addObject("mensagem", "Título salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@ModelAttribute("todosStatusTitulo")
