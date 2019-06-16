02e5489b2a22086ee45258eb4ad3dd2c459da3b8
Rafael Barros <rafaelbarros.df@gmail.com>
2019-06-16 12:18:50 -0300

    :recycle: Refactoring TituloService

--- src/main/java/com/algaworks/cobranca/service/TituloService.java
+++ src/main/java/com/algaworks/cobranca/service/TituloService.java
@@ -2,45 +2,14 @@ package com.algaworks.cobranca.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
-
 import com.algaworks.cobranca.model.Titulo;
-import com.algaworks.cobranca.model.enums.StatusTitulo;
-import com.algaworks.cobranca.repository.TituloRepository;
 import com.algaworks.cobranca.repository.filter.TituloFilter;
 
-@Service
-public class TituloService {
+public interface TituloService {
 	
+	public void salvar(Titulo titulo);
+	public void excluir(Long codigo);
+	public String receber(Long codigo);
+	public List<Titulo> filtrar(TituloFilter filtro);
 	
-	@Autowired
-	private TituloRepository  tituloRepository;
-	
-	public void salvar(Titulo titulo) {
-		try {
-			tituloRepository.save(titulo);
-		} catch (DataIntegrityViolationException e) {
-			throw new IllegalArgumentException("Formato de data inválido");
-		}
-	}
-
-	public void excluir(Long codigo) {
-		tituloRepository.delete(codigo);
-	}
-
-	public String receber(Long codigo) {
-		Titulo titulo = tituloRepository.findOne(codigo);
-		titulo.setStatus(StatusTitulo.RECEBIDO);
-		tituloRepository.save(titulo);
-		
-		return StatusTitulo.RECEBIDO.getDescricao();
-	}
-	
-	public List<Titulo> filtrar(TituloFilter filtro) {
-		String descricao = filtro.getDescricao() == null ? "" : filtro.getDescricao();
-		return tituloRepository.findByDescricaoContaining(descricao);
-	}
-
 }
